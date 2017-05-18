2b28dbe2fd4cbb785cfdcdc6c73ea0e20ae347a1
Kevin Martinez <msi03@laposte.net>
2017-05-18 13:38:17 +0200

    Dans ma valise il y a un poussin avec une sulfateuse, un Aymeric, un lutin noir, un liquide vaisselle au Nutella, du RedBull, une capote périmée et percée, et chinois de la mort qui tue

--- app/Views/templates/default.php
+++ app/Views/templates/default.php
@@ -1,49 +1,57 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../../favicon.ico">
-
-    <title><?= App::getInstance()->title; ?></title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet">
-
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Le Guide du Bon Homophobe</title>
+  <link rel="stylesheet" href="style/style.css">
+  <link rel="stylesheet" href="style/normalize.css">
+  <link rel="stylesheet" href="https://cdn.rawgit.com/konpa/devicon/4f6a4b08efdad6bb29f9cc801f5c07e263b39907/devicon.min.css">
 </head>
-
 <body>
+  <div id="header">
 
-<nav class="navbar navbar-inverse navbar-fixed-top">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="index.php">Contre l'exclusion</a>
+    <div  id="navmobile" class="hidden">
+      <div class="menu-icon" onclick="navMobile()">
+          <span></span>
+      </div>
+      <ul class="nav navbar-nav navbar-left">
+        <li><a href="index.php?p=posts.blog">Blog</a></li>
+        <li><a href="index.php?p=associations.help">Aide / Assoces</a></li>
+        <li><a href="index.php?p=posts.allTestimonies">Témoignage</a></li>
+        <!-- <li><a href="index.php?p=admin.posts.adminTestimony">Validation Témoignage ADMIN</a></li> -->
+        <li><a href="#">Campagne</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+         <li><a href="index.php?p=users.login">Connexion</a></li>
+      </ul>
     </div>
 
-    <ul class="nav navbar-nav navbar-left">
-      <li><a href="index.php?p=posts.blog">Blog</a></li>
-      <li><a href="index.php?p=associations.help">Aide / Assoces</a></li>
-      <li><a href="index.php?p=posts.allTestimonies">Témoignage</a></li>
-      <li><a href="index.php?p=admin.posts.adminTestimony">Validation Témoignage ADMIN</a></li>
-      <li><a href="">Campagne</a></li>
-    </ul>
-    <ul class="nav navbar-nav navbar-right">
-       <li><a href="index.php?p=users.login">Connexion</a></li>
-    </ul>
+    <img src="style/img/logo.png" alt="LGBH" id="logo">
+
   </div>
-</nav>
-
-<div class="container">
-
-    <div class="starter-template" style="padding-top: 100px;">
+  <div id="content">
+       
         <?= $content; ?>
+
+</div>
+  <div id="footer">
+    <div id="plan-site">
+      <ul class="nav navbar-nav navbar-left">
+        <li><a href="index.php?p=posts.blog">Blog</a></li>
+        <li><a href="index.php?p=associations.help">Aide / Assoces</a></li>
+        <li><a href="index.php?p=posts.allTestimonies">Témoignage</a></li>
+        <!-- <li><a href="index.php?p=admin.posts.adminTestimony">Validation Témoignage ADMIN</a></li> -->
+        <li><a href="">Campagne</a></li>
+      </ul>
     </div>
-
-</div><!-- /.container -->
-
-
+    <h5>Crée par LaFabrik du Numérique | Copyright @ 2017 La Fabrik</h5>
+    <div id="icone-rs">
+      <i class="devicon-facebook-plain"></i>
+      <i class="devicon-twitter-plain"></i>
+    </div>
+  </div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
 </body>
 </html>
