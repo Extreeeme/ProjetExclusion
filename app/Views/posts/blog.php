1f9c94c6338d720a8ae3e6ab2f907d67f89f73f5
Sethisfir <micka_8@hotmail.fr>
2017-05-18 16:51:06 +0200

    design

--- app/Views/posts/blog.php
+++ app/Views/posts/blog.php
@@ -1,14 +1,17 @@
+<div id="blog">
 <?php foreach ($pagination as $article) : ?>
 <h1> <a href="index.php?p=posts.showArticle&id=<?=$article->id?>"><?= $article->title?></a></h1>
-<p><?= $article->date_article_fr ?></p>
+<p class="infos-blog"><?= $article->date_article_fr ?></p>
 <p><?= $article->text ?></p>
 <?php endforeach; ?>
 
 <?php
 $customPagination = 0;
 while ($customPagination < $nbpage) : ?>
- 	<span><a href="index.php?p=posts.blog&page=<?=($customPagination+1)?>"><?=($customPagination+1)?></a>|</span>
+ 	<span><a href="index.php?p=posts.blog&page=<?=($customPagination+1)?>"><?=($customPagination+1)?></a></span>
 
 	<?php
  		$customPagination++;
  endwhile; ?>
+</div>
+
---
+++ app/Views/posts/campagne.php
@@ -0,0 +1,23 @@
+<div id="texte">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+		<!-- Ecrire ici -->
+	</div>
+	<div id="eddycarrousel">
+		<img src="http://cdn2.business2community.com/wp-content/uploads/2015/02/Movie_star_Eddie_Murphy.jpg" alt="responsive-image" width="480px" height="270px">
+	</div>
