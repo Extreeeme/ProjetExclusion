e0a3bc78885f9d4dd336d56e72aae7eecab12262
Sethisfir <micka_8@hotmail.fr>
2017-05-19 11:58:45 +0200

    Carrousel

--- app/Views/posts/campagne.php
+++ app/Views/posts/campagne.php
@@ -19,7 +19,34 @@
 	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
 		<!-- Ecrire ici -->
-	<div id="eddycarrousel">
-		<img src="http://cdn2.business2community.com/wp-content/uploads/2015/02/Movie_star_Eddie_Murphy.jpg" alt="responsive-image" width="100%">
-	</div>
+	<div class="container">
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+      <div class="item active">
+        <img src="img/Affiche1.png" alt="Los Angeles" style="width:100%;">
+      </div>
+
+      <div class="item">
+        <img src="img/Affiche2.png" alt="Chicago" style="width:100%;">
+      </div>
+    
+      <div class="item">
+        <img src="img/Affiche3.png" alt="New york" style="width:100%;">
+      </div>
+
+      <div class="item">
+        <img src="img/Affiche4.png" alt="New york" style="width:100%;">
+      </div>
+    </div>
+  </div>
+</div>
+
 </div>
